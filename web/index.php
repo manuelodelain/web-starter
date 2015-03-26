fbb9e9fe19a29c72a3b93d34b5cf8e8ee535acb1
Manuel Odelain <manu.odelain@gmail.com>
2015-03-26 15:38:19 +0100

    structure

---
+++ web/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	
+</body>
+</html>
