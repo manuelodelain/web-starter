da349d2ce52c0ca792d12f2b8fc70a4c2338fc86
Manuel Odelain <manu.odelain@gmail.com>
2015-03-27 17:43:10 +0100

    gulp sass sourcemaps

--- build/gulp/tasks/styles.js
+++ build/gulp/tasks/styles.js
@@ -3,10 +3,13 @@ var sass = require('gulp-sass');
 var config = require('../config');
 var options = require('../options');
 var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
  
 gulp.task('styles', function () {
   gulp.src(config.sass.src)
+    .pipe(options.debug ? sourcemaps.init() : util.noop())
     .pipe(sass())
     .pipe(autoprefixer(config.autoprefixer))
+    .pipe(options.debug ? sourcemaps.write() : util.noop())
     .pipe(gulp.dest(config.sass.dest));
 });
