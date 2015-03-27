8bddc78e8f6b6c083a2e366b63b12b1e1afbfeb0
Manuel Odelain <manu.odelain@gmail.com>
2015-03-27 10:53:56 +0100

    php base utils

--- web/index.php
+++ web/index.php
@@ -1,12 +1,22 @@
+<?php require_once('scripts/functions.php'); ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
-	<link rel="stylesheet" href="css/main.css">
-</head>
-<body>
-	
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale = 1.0, user-scalable=no">
+		<!--[if IE]>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<![endif]-->
+		<title></title>
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+	<body>
+		<script type="text/javascript">
+            var ENV = <?php echo ENV ?>;
+            var BASE_URL = "<?php echo BASE_URL ?>";
+        </script>
+		<script src="js/scripts.js"></script>
+	</body>
 </html>
