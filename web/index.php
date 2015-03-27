2679a42122d7253d235a66007b2f958d0d0ceae9
Manuel Odelain <manu.odelain@gmail.com>
2015-03-27 11:51:58 +0100

    sass base

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="description" content="">
   <meta name="keywords" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale = 1.0, user-scalable=no">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale = 1.0, user-scalable=no">
   <!--[if IE]>
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <![endif]-->
