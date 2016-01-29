33cf16e1c95d83aa43f6cf86340a97bc9c75db6b
ManuelOdelain <hello@manuelodelain.com>
2016-01-29 11:44:43 +0100

    images task w/ target

--- build/gulp/tasks/images.js
+++ build/gulp/tasks/images.js
@@ -1,18 +1,16 @@
 var gulp = require('gulp');
-var config = require('../config');
 var options = require('../options');
+var target = require('../config').targets[options.target];
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var newer = require('gulp-newer');
 
 gulp.task('images', function() {
-  config.images.forEach(function(item){
-    gulp.src(item.src)
-      .pipe(newer(item.dest))
-      .pipe(imagemin({
-        progressive: true,
-        use: [pngquant()]
-      }))
-      .pipe(gulp.dest(item.dest));
-  });
+  return  gulp.src(target.images.src)
+    .pipe(newer(target.images.dest))
+    .pipe(imagemin({
+      progressive: true,
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest(target.images.dest));
 });
