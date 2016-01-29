354d7544f3ea0bcb555354ff0661a8dce24d1a53
ManuelOdelain <hello@manuelodelain.com>
2016-01-29 11:38:57 +0100

    add target to build config

--- build/gulp/config.js
+++ build/gulp/config.js
@@ -4,29 +4,36 @@ var baseDest = './web';
 module.exports = {
   src: baseSrc,
   dest: baseDest,
-  sass: [
-    {
-      src: [
-        baseSrc + '/sass/**/*.scss'
-      ],
-      dest: baseDest + '/assets/css'
-    }
-  ],
-  js: [
-    {
-      entries: [
-        baseSrc + '/js/main.js'
-      ],
-      dest: baseDest + '/assets/js',
-      filename: 'scripts'
-    }
-  ],
-  images: [
-    {
-      src: [
-        baseSrc + '/img/**/*.{jpg,gif,png,svg}'
-      ],
-      dest: baseDest + '/assets/img'
-    }
-  ]
+  targets: {
+    default: {
+      sass: {
+        src: baseSrc + '/sass/**/*.scss',
+        dest: baseDest + '/assets/css'
+      },
+      js: {
+        entries: baseSrc + '/js/main.js',
+        dest: baseDest + '/assets/js',
+        filename: 'scripts'
+      },
+      images: {
+        src: [
+          baseSrc + '/img/**/*.{jpg,gif,png,svg}', 
+          '!' + baseSrc + '/img/sprite/**', 
+        ],
+        dest: baseDest + '/assets/img'
+      },
+      sprite: {
+        src: [
+          baseSrc + '/img/sprite/*.svg'
+        ],
+        template: './app/templates/partials/sprite-template.twig',
+        destFile: 'sprite.twig',
+        dest: './app/templates/partials'
+      },
+      sync: {
+        proxy: 'my-project.dev'
+      }
+    },
+    mobile: {}
+  }
 };
