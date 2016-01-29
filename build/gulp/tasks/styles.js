38cee0a66b241e837f8a282e1ef468160471f1f4
ManuelOdelain <hello@manuelodelain.com>
2016-01-29 11:47:53 +0100

    styles task w/ target

--- build/gulp/tasks/styles.js
+++ build/gulp/tasks/styles.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var config = require('../config');
 var options = require('../options');
+var target = require('../config').targets[options.target];
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var plumber = require('gulp-plumber');
@@ -11,18 +11,16 @@ var minifyCss = require('gulp-minify-css');
 var browserSync = require('browser-sync');
 
 gulp.task('styles', function () {
-  config.sass.forEach(function(item){
-    gulp.src(item.src)
-      .pipe(plumber({errorHandler: errorNotif}))
-      .pipe(gIf(options.debug, sourcemaps.init()))
-      .pipe(sass())
-      .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-      }))
-      .pipe(gIf(options.debug, sourcemaps.write()))
-      .pipe(gIf(options.minify, minifyCss()))
-      .pipe(gulp.dest(item.dest))
-      .pipe(browserSync.stream());
-  });
+  return gulp.src(target.sass.src)
+    .pipe(plumber({errorHandler: errorNotif}))
+    .pipe(gIf(options.debug, sourcemaps.init()))
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gIf(options.debug, sourcemaps.write()))
+    .pipe(gIf(options.minify, minifyCss()))
+    .pipe(gulp.dest(target.sass.dest))
+    .pipe(browserSync.stream());
 });
