0a6c53e0958c20f0c8c4be28aebc73f0e8c3b540
ManuelOdelain <hello@manuelodelain.com>
2016-01-29 11:39:56 +0100

    scripts task w/ target

--- build/gulp/tasks/scripts.js
+++ build/gulp/tasks/scripts.js
@@ -1,45 +1,47 @@
 var gulp = require('gulp');
-var config = require('../config');
 var options = require('../options');
+var target = require('../config').targets[options.target];
 var errorNotif = require('../utils/error-notif');
 var browserify = require('browserify');
 var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
-var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var gIf = require('gulp-if');
 var size = require('gulp-size');
 var duration = require('gulp-duration');
+var babelify = require('babelify');
 
 gulp.task('scripts', function(){
-  config.js.forEach(function(item){
-    var bundler = browserify({
-      entries: item.entries,
-      debug: options.debug
-    });
-
-    if (options.watch){
-      bundler = watchify(bundler);
-      bundler.on('update', bundle);
-    }
-
-    function bundle(){
-      return bundler.bundle()
-        .on('error', errorNotif)
-        .pipe(source(item.dest + '/' + item.filename + '.js'))
-        .pipe(gIf(options.debug, buffer()))
-        .pipe(gIf(options.debug, sourcemaps.init()))
-        .pipe(gIf(options.debug, sourcemaps.write()))
-        .pipe(gIf(options.minify, buffer()))
-        .pipe(gIf(options.minify, uglify()))
-        .pipe(duration('bundle time'))
-        .pipe(size({
-          title: 'bundle size'
-        }))
-        .pipe(gulp.dest('./'));
-    }
-
-    bundle();
+  var bundler = browserify({
+    entries: target.js.entries,
+    cache: {},
+    packageCache: {},
+    debug: options.debug
+  }).transform(babelify, {
+    presets: ['es2015-loose'],
+    only: ['./dev/js/**'],
+    global: true
   });
+
+  if (options.watch){
+    bundler = watchify(bundler);
+    bundler.on('update', bundle);
+  }
+
+  function bundle(){
+    return bundler
+      .bundle()
+      .on('error', errorNotif)
+      .pipe(source(target.js.filename + '.js'))
+      .pipe(buffer())
+      .pipe(gIf(options.minify, uglify()))
+      .pipe(duration('bundle time'))
+      .pipe(size({
+        title: 'bundle size'
+      }))
+      .pipe(gulp.dest(target.js.dest + '/'));
+  }
+
+  return bundle();
 });
