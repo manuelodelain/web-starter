61c47e4287d91bb9956f0b6fb6f6f95edc5cb0c6
ManuelOdelain <hello@manuelodelain.com>
2016-01-29 11:44:04 +0100

    build task w/ target

--- build/gulp/tasks/build.js
+++ build/gulp/tasks/build.js
@@ -1,16 +1,21 @@
 var gulp = require('gulp');
-var config = require('../config');
 var options = require('../options');
+var target = require('../config').targets[options.target];
+var watch = require('gulp-watch');
  
 gulp.task('build', ['clean', 'styles', 'scripts', 'images'], function(){
   if (options.watch){
-    config.sass.forEach(function(item){
-      gulp.watch(item.src, ['styles']);
+    watch(target.sass.src, function(){
+      gulp.start('styles');
     });
 
-    config.images.forEach(function(item){
-      gulp.watch(item.src, ['images']);
+    watch(target.images.src, function(){
+      gulp.start('images');
     });
+
+    // watch(target.sprite.src, function(){
+    //   gulp.start('sprite');
+    // });
 
     gulp.start('sync');
   }
