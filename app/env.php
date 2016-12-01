2577f6a910355a59c6cd007567949dc1ad2b275b
ManuelOdelain <hello@manuelodelain.com>
2016-12-01 10:23:39 +0100

    add staging host to config

--- app/env.php
+++ app/env.php
@@ -1,13 +1,17 @@
 <?php
 
 // define app environment
-if (@$_SERVER["APPLICATION_ENV"] == "local" 
-  || strpos($_SERVER["HTTP_HOST"],  "localhost") !== false 
-  || strpos($_SERVER["HTTP_HOST"],  "127.0.0.1") !== false) 
+if (
+  @$_SERVER["APPLICATION_ENV"] == "local" || 
+  strpos($_SERVER["HTTP_HOST"],  "localhost") !== false || 
+  strpos($_SERVER["HTTP_HOST"],  "127.0.0.1") !== false
+  ) 
 {
   $env = 'dev';
-} else if (strpos(@$_SERVER["SERVER_ENV"], "staging") !== false 
-  || strpos($_SERVER["HTTP_HOST"],  "manuelodelain.com") !== false)
+} else if (
+  strpos(@$_SERVER["SERVER_ENV"], "staging") !== false || 
+  (!empty(STAGING_HOST) && strpos($_SERVER["HTTP_HOST"],  STAGING_HOST) !== false)
+  )
 {
   $env = 'staging';
 } else {
