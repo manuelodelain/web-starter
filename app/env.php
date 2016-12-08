dc7bb9fce2e6e9e55f8896aa0fd76b8b27e1835f
ManuelOdelain <hello@manuelodelain.com>
2016-12-08 15:41:04 +0100

    fix define environment

--- app/env.php
+++ app/env.php
@@ -2,15 +2,15 @@
 
 // define app environment
 if (
-  @$_SERVER["APPLICATION_ENV"] == "local" || 
-  strpos($_SERVER["HTTP_HOST"],  "localhost") !== false || 
-  strpos($_SERVER["HTTP_HOST"],  "127.0.0.1") !== false
+  @$_SERVER['APPLICATION_ENV'] == 'local' || 
+  strpos($_SERVER['HTTP_HOST'],  'localhost') !== false || 
+  strpos($_SERVER['HTTP_HOST'],  '127.0.0.1') !== false
   ) 
 {
   $env = 'dev';
 } else if (
-  strpos(@$_SERVER["SERVER_ENV"], "staging") !== false || 
-  (!empty(STAGING_HOST) && strpos($_SERVER["HTTP_HOST"],  STAGING_HOST) !== false)
+  strpos(@$_SERVER['SERVER_ENV'], 'staging') !== false || 
+  defined(STAGING_HOST) && strpos($_SERVER['HTTP_HOST'],  STAGING_HOST) !== false
   )
 {
   $env = 'staging';
