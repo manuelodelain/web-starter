5dda0d873007da6a559450a4bda6037f69b4bd3b
ManuelOdelain <hello@manuelodelain.com>
2019-02-25 15:55:52 +0100

    webpack: add prod build WIP

--- build/webpack/config/prod.js
+++ build/webpack/config/prod.js
@@ -1,36 +1,25 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: path.resolve(__dirname, '../../../dev/js/main.js'),
   output: {
     filename: 'scripts.js',
-    path: path.resolve(__dirname, '../../../web/assets/js')
-  },
-  devServer: {
-    contentBase:  path.resolve(__dirname, '../../../web/'),
-    publicPath: '/assets/js/',
-    host: '0.0.0.0',
-    port: '3000',
-    proxy: {
-      '/': {
-        'target': {
-          'host': 'starter.test',
-          'protocol': 'http:',
-          'port': 80
-        },
-        ignorePath: true,
-        changeOrigin: true,
-        secure: false
-      }
-    }
+    // publicPath: path.resolve(__dirname, '../../../web/'),
+    path: path.resolve(__dirname, '../../../web/assets/js/')
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: [
-          "style-loader",
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../css/'
+            }
+          },
           "css-loader",
           "sass-loader"
         ]
@@ -46,5 +35,8 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin()
+  ]
 };
