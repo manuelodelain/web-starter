80b4e425ac7da633c1111425e9accbf726c8b7ae
ManuelOdelain <hello@manuelodelain.com>
2019-03-08 11:20:02 +0100

    webpack: split each functionalities (sass, scripts, images, ...)

--- webpack.config.js
+++ webpack.config.js
@@ -16,137 +16,227 @@ const imageminSvgo = require("imagemin-svgo");
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const mode = process.env.NODE_ENV;
+const buildType = process.env.BUILD_TYPE;
 
-const config = {
-  mode,
-  entry: [
-    path.resolve(__dirname, './dev/js/main.js'),
-    path.resolve(__dirname, './dev/sass/main.scss'),
-  ],
-  output: {
-    filename: 'assets/js/scripts.[contenthash].js',
-    path: path.resolve(__dirname, './web'),
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: mode === 'development' ? "style-loader" : MiniCssExtractPlugin.loader,
-            options: {
-              sourceMap: mode === 'development'
-            }
-          },
-          { loader: 'css-loader', options: { sourceMap: true } },
-          { loader: 'postcss-loader', options: { 
-            sourceMap: true,
-            plugins: [
-              require('autoprefixer')
-            ]
-          } },
-          { loader: 'sass-loader', options: { sourceMap: true } }
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  },
-  plugins: [
-    new CopyPlugin([
-      {from: 'static'},
-    ]),
-    new HtmlWebpackPlugin({
-      template: './app/templates/inject/scripts-template.twig',
-      filename: '../app/templates/inject/scripts.twig',
-      inject: false,
-      generatedWarning: 'dynamically generated - do not modify',
-    }),
-    new HtmlWebpackPlugin({
-      template: './app/templates/inject/styles-template.twig',
-      filename: '../app/templates/inject/styles.twig',
-      inject: false,
-      generatedWarning: 'dynamically generated - do not modify'
-    })
-  ],
-};
+const MODERN_BUILD_TYPE = 'modern';
+const LEGACY_BUILD_TYPE = 'legacy';
 
-if (mode === 'development') {
-  config.devServer = {
-    contentBase:  path.resolve(__dirname, './web/'),
-    // publicPath: '/assets/js/',
-    host: '0.0.0.0',
-    port: '3000',
-    proxy: {
-      '/': {
-        'target': {
-          'host': 'starter.test',
-          'protocol': 'http:',
-          'port': 80
-        },
-        ignorePath: true,
-        changeOrigin: true,
-        secure: false
-      }
-    },
-    writeToDisk: true
-  };
-} else {
-  config.plugins.push(
-    new MiniCssExtractPlugin({
-      filename: mode === 'development' ? "assets/css/[name].css" : "assets/css/[name].[contenthash].css",
-    }),
-    new ImageminPlugin({
-      name: "assets/img/[name].[ext]",
-      imageminOptions: {
-        plugins: [
-          imageminGifsicle({
-            interlaced: true
-          }),
-          imageminMozjpeg({
-            quality: 70,
-            progressive: true
-          }),
-          imageminOptipng({
-            optimizationLevel: 5
-          }),
-          imageminSvgo({
-            removeViewBox: true
-          })
-        ]
-      }
-    })
+const merge = require('webpack-merge');
+
+const TerserPlugin = require('terser-webpack-plugin');
+
+const getConfig = (buildType) => {
+  let config = merge(
+    getBaseConfig(buildType),
+    getScriptsConfig(),
   );
 
-  config.optimization = {
-    splitChunks: {
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          chunks: 'all',
-          filename: 'assets/js/vendors.[contenthash].js',
+  if (mode === 'development') {
+    config = merge(config, getDevServerConfig());
+  } 
+  
+  if (mode === 'development' || buildType === MODERN_BUILD_TYPE) {
+    config = merge(
+      config, 
+      getCleanConfig(),
+      getStaticConfig(),
+      getImageConfig(),
+      getSassConfig()
+    );
+  }
+
+  return config;
+};
+
+const getBaseConfig = (buildType) => {
+  return {
+    mode,
+    entry: [
+      path.resolve(__dirname, './dev/js/main.js'),
+      path.resolve(__dirname, './dev/sass/main.scss'),
+    ],
+    output: {
+      filename: `assets/js/${buildType === MODERN_BUILD_TYPE ? 'scripts' : 'legacy'}.[contenthash].js`,
+      path: path.resolve(__dirname, './web'),
+      publicPath: '/',
+    },
+  }
+};
+
+const getSassConfig = () => {
+  let config = {
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: mode === 'development' ? "style-loader" : MiniCssExtractPlugin.loader,
+              options: {
+                sourceMap: mode === 'development'
+              }
+            },
+            { loader: 'css-loader', options: { sourceMap: true } },
+            { loader: 'postcss-loader', options: { 
+              sourceMap: true,
+              plugins: [
+                require('autoprefixer')
+              ]
+            } },
+            { loader: 'sass-loader', options: { sourceMap: true } }
+          ]
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: mode === 'development' ? "assets/css/[name].css" : "assets/css/[name].[contenthash].css",
+      }),
+    ],
+    optimization: {
+      minimizer: [
+        new OptimizeCssAssetsPlugin({})
+      ]
+    }
+  };
+
+  return config;
+};
+
+const getScriptsConfig = () => {
+  let config = {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './app/templates/inject/scripts-template.twig',
+        filename: '../app/templates/inject/scripts.twig',
+        inject: false,
+        generatedWarning: 'dynamically generated - do not modify',
+      }),
+      new HtmlWebpackPlugin({
+        template: './app/templates/inject/styles-template.twig',
+        filename: '../app/templates/inject/styles.twig',
+        inject: false,
+        generatedWarning: 'dynamically generated - do not modify'
+      })
+    ],
+    optimization: {
+      minimizer: [
+        new TerserPlugin()
+      ],
+      splitChunks: {
+        cacheGroups: {
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            chunks: 'all',
+            filename: 'assets/js/vendors.[contenthash].js',
+          }
+        },
+      }
+    },
+  };
+
+  return config;
+};
+
+const getStaticConfig = () => {
+  return {
+    plugins: [
+      new CopyPlugin([
+        {from: 'static'},
+      ])
+    ],
+  };
+}
+
+const getDevServerConfig = () => {
+  return {
+    devServer: {
+      contentBase:  path.resolve(__dirname, './web/'),
+      // publicPath: '/assets/js/',
+      host: '0.0.0.0',
+      port: '3000',
+      proxy: {
+        '/': {
+          'target': {
+            'host': 'starter.test',
+            'protocol': 'http:',
+            'port': 80
+          },
+          ignorePath: true,
+          changeOrigin: true,
+          secure: false
         }
       },
-    },
-    minimizer: [
-      new CleanWebpackPlugin([
-        'web/**/*.*'
-      ], {
-        root: path.resolve(__dirname, './'),
-        exclude: []
-      }),
-      new OptimizeCssAssetsPlugin({}),
+      writeToDisk: true
+    }
+  };
+};
+
+const getImageConfig = () => {
+  return {
+    plugins: [
+      new ImageminPlugin({
+        name: "assets/img/[name].[ext]",
+        imageminOptions: {
+          plugins: [
+            imageminGifsicle({
+              interlaced: true
+            }),
+            imageminMozjpeg({
+              quality: 70,
+              progressive: true
+            }),
+            imageminOptipng({
+              optimizationLevel: 5
+            }),
+            imageminSvgo({
+              removeViewBox: true
+            })
+          ]
+        }
+      })
     ]
   };
+};
+
+const getCleanConfig = () => {
+  return {
+    optimization: {
+      minimizer: [
+        new CleanWebpackPlugin([
+          'web/**/*.*'
+        ], {
+          root: path.resolve(__dirname, './'),
+          exclude: []
+        }),
+      ]
+    }
+  }
+};
+
+let config;
+
+if (mode === 'developmennt') {
+  config = getConfig(buildType);
+} else {
+  config = [
+    getConfig(MODERN_BUILD_TYPE),
+    // getConfig(LEGACY_BUILD_TYPE)
+  ];
 }
 
 module.exports = config;
