e64b545f2034576fd881c0edca59c81c1946ac20
ManuelOdelain <hello@manuelodelain.com>
2019-07-30 14:30:16 +0200

    build > clean: fix import

--- build/webpack/clean.js
+++ build/webpack/clean.js
@@ -1,15 +1,10 @@
-const path = require('path');
-
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = () => {
   return {
     plugins: [
-      new CleanWebpackPlugin([
-        'web/**/*.*'
-      ], {
-        root: path.resolve(__dirname, '../../'),
-        exclude: []
+      new CleanWebpackPlugin({
+        cleanStaleWebpackAssets: false
       }),
     ]
   }
