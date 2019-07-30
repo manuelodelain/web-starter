9755198306584479c51216bf42bf79c18e657b39
ManuelOdelain <hello@manuelodelain.com>
2019-07-30 15:01:58 +0200

    use dotenv

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,34 +1,50 @@
 <?php
 
-require __DIR__ . '/config.php';
-require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/env.php';
+require __DIR__.'/vendor/autoload.php';
 
-// // create app
+use Symfony\Component\Dotenv\Dotenv;
+
+// load environment variables
+$dotenv = new Dotenv();
+$dotenv->loadEnv(__DIR__.'/../.env');
+
+// app settings
 $config = [
   'settings' => [
-    'displayErrorDetails' => $env != 'prod',
+    'displayErrorDetails' => $_ENV['DEBUG'] == 1,
   ],
 ];
-$app = new \Slim\App($config);
 
-// get container
-$container = $app->getContainer();
+// container
+$container = new \Slim\Container($config);
+
+//Override the default Not Found Handler before creating App
+// $container['notFoundHandler'] = function ($container) {
+//   return new \src\pages\BasePage($container);
+// };
+
+// create app
+$app = new \Slim\App($container);
 
 // init template engine
 $container['view'] = function ($container) {
   global $env, $app;
 
+  $templatesPath = __DIR__.'/templates';
+  $assetsPath = __DIR__.'/../'.$_ENV['WEB_DIR'].'/assets';
+  $templatesCachePath = __DIR__.'/'.$_ENV['TEMP_DIR'].'/templates/cache';
+
   $settings = [
-    'cache' => $env != 'prod' ? false : TEMPLATES_CACHE_PATH,
-    'debug' => $env != 'prod',
+    'cache' => $_ENV['TEMPLATES_CACHE'] == 1 ? $templatesCachePath : false,
+    'debug' => $_ENV['DEBUG'] == 1,
     'strict_variables' => false,
   ];
-  $view = new \Slim\Views\Twig([TEMPLATES_PATH, ASSETS_PATH], $settings);
+
+  $view = new \Slim\Views\Twig([$templatesPath, $assetsPath], $settings);
   $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
 
   $view->addExtension(new \Slim\Views\TwigExtension($container['router'], $basePath));
-  $view->addExtension(new manuelodelain\Twig\Extension\SvgExtension(ASSETS_PATH));
+  $view->addExtension(new manuelodelain\Twig\Extension\SvgExtension($assetsPath));
 
   if ($settings['debug']){
     $view->addExtension(new Twig_Extension_Debug());
